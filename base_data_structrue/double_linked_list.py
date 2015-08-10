032a0ffac975ba0f13946732a3d49b867c891d66
dodoru <nreazy013@gmail.com>
2015-08-10 23:32:12 +0800

    unfinish

--- base_data_structrue/double_linked_list.py
+++ base_data_structrue/double_linked_list.py
@@ -10,14 +10,35 @@ class Node():
         return repr(self.element)
 
 class DoubleLinkedList():
-    def __init__(self):
+    def __init__(self,tail=None,count=0):
+        self.head=Node()
+        self.tail=tail
+        self.total=count
+
+    def __repr__(self):
         pass
+
 
     def is_empty(self):
-        pass
+        return self.head.rear is Node
+        # return self.total
 
-    def append(self):
-        pass
+    def append(self,element):
+        node=Node(element=element)
+        if self.is_empty():
+            self.head.next=node
+            node.prev=self.head
+
+            self.tail=node
+            self.total+=1
+
+        else:
+            self.tail.next=node
+            node.prev=self.tail
+
+            self.tail=node
+            self.total+=1
+
 
     def insert_after(self):
         pass
