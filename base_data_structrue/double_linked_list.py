73c19513ab868eb345630a9af8fd6081ea29da1e
dodoru <nreazy013@gmail.com>
2015-08-10 17:25:10 +0800

    start

---
+++ base_data_structrue/double_linked_list.py
@@ -0,0 +1,42 @@
+# coding:utf-8
+
+class Node():
+    def __init__(self,element=None,prev=None,rear=None):
+        self.element=element
+        self.prev=prev
+        self.rear=rear
+
+    def __repr__(self):
+        return repr(self.element)
+
+class DoubleLinkedList():
+    def __init__(self):
+        pass
+
+    def is_empty(self):
+        pass
+
+    def append(self):
+        pass
+
+    def insert_after(self):
+        pass
+
+    def insert_before(self):
+        pass
+
+    def delete(self):
+        pass
+
+    def find(self):
+        pass
+
+    def count(self):
+        pass
+
+    def first_node(self):
+        pass
+
+    def last_node(self):
+        pass
+
