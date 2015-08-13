e5e51401b702f46fffa9ca3f9bbf08dfb69b2db4
dodoru <nreazy013@gmail.com>
2015-08-13 17:33:45 +0800

    DoubleLinkedList, test ok.

--- base_data_structrue/double_linked_list.py
+++ base_data_structrue/double_linked_list.py
@@ -17,28 +17,28 @@ class DoubleLinkedList():
         self.total = count
 
     def __repr__(self):
-        description=' DoubleLinkedList: '
-        node=self.head.rear
-        while node:
-            description+=(''+repr(node.element))
-            node=node.rear
+        description = ' DoubleLinkedList: '
+        node = self.head.rear
+        while node is not None:
+            description += (' ' + repr(node.element))
+            node = node.rear
         return description
 
     def is_empty(self):
-        # return self.head.rear is None
-        return self.total
+        return self.head.rear is None
+        # return self.total
 
     def append(self, element):
         node = Node(element=element)
         if self.is_empty():
-            self.head.next = node
+            self.head.rear = node
             node.prev = self.head
 
             self.tail = node
             self.total += 1
 
         else:
-            self.tail.next = node
+            self.tail.rear = node
             node.prev = self.tail
 
             self.tail = node
@@ -53,53 +53,55 @@ class DoubleLinkedList():
             index_node = index_node.rear
         return None
 
-    # delete every node while node.element==element
-    def delete(self, element):
-        if self.tail.element == element:
-            self.tail = self.tail.prev
-            self.tail.rear = None
-            self.total -= 1
-        else:
-            index_node = self.head.rear
-            while index_node:
-                if index_node.element == element:
-                    index_node.prev.rear = index_node.rear
-                    index_node.rear.prev = index_node.prev
-                    self.total -= 1
-                    # if return , only delete one, else delete all.
-                else:
-                    index_node = index_node.rear
-
-    # remove only the first node while node.element==element
     def remove(self, element):
-        if self.tail.element == element:
-            self.tail = self.tail.prev
-            self.tail.rear = None
-            self.total -= 1
-        else:
-            index_node = self.find(element)
-            if index_node:
+        index_node = self.find(element)
+        if index_node:
+            if index_node == self.tail:
+                self.tail = self.tail.prev
+                self.tail.rear = None
+                self.total -= 1
+            else:
                 index_node.prev.rear = index_node.rear
                 index_node.rear.prev = index_node.prev
                 self.total -= 1
-            else:
-                print("no such element. ")
+        else:
+            print (" no such element")
+
+
+    # delete every node while node.element==element
+    def deletes(self, element):
+        index_node = self.head.rear
+        while index_node:
+            if index_node.element == element:
+                if index_node == self.tail:
+                    self.tail = self.tail.prev
+                    self.tail.rear = None
+                    self.total -= 1
+                else:
+                    index_node.prev.rear = index_node.rear
+                    index_node.rear.prev = index_node.prev
+                    self.total -= 1
+            index_node = index_node.rear
+
 
     def insert_after(self, index_element, insert_element):
+        index_node = self.find(index_element)
         insert_node = Node(element=insert_element)
-        if self.tail.element == index_element:
-            self.tail.rear = insert_node
-            insert_node.prev = self.tail
-            self.tail = insert_node
-        else:
-            index_node = self.find(index_element)
-            if index_node:
+        if index_node:
+            if index_node == self.tail:
+                # self.append(insert_element)
+                self.tail.rear = insert_node
+                insert_node.prev = self.tail
+                self.tail = insert_node
+                self.total += 1
+            else:
                 insert_node.prev = index_node
                 insert_node.rear = index_node.rear
                 index_node.rear.prev = insert_node
                 index_node.rear = insert_node
-            else:
-                print "can't find index_element."
+                self.total += 1
+        else:
+            print "can't find index_element."
 
 
     def insert_before(self, index_element, insert_element):
@@ -110,6 +112,7 @@ class DoubleLinkedList():
             insert_node.prev = index_node.prev
             index_node.prev.rear = insert_node
             index_node.prev = insert_node
+            self.total += 1
         else:
             print "can't find index_element. "
 
@@ -130,3 +133,37 @@ class DoubleLinkedList():
 
     def count(self):
         return self.total
+
+
+def test():
+    l=DoubleLinkedList()
+    l.append(1)
+    l.append(2)
+    l.append(3)
+    l.append(4)
+    l.append(5)
+    l.append(5)
+    l.append(4)
+    l.append(6)
+    l.append(4)
+    print l
+    l.remove(2)
+    print l
+    l.remove(4)
+    print l
+    l.deletes(1)
+    print l
+    l.deletes(5)
+    print l
+    l.deletes(4)
+    print l
+    l.insert_after(3,1)
+    l.insert_after(2,1)
+    print l
+    l.insert_before(4,1)
+    l.insert_before(1,4)
+    print l
+
+
+if __name__ == "__main__":
+    test()
